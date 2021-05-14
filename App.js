de3ed591c375ad224214ec2bcac3818dafa53567
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2021-05-14 11:56:39 -0300

    Toda a parte da navegação mais um pouco de style para a mesma

--- App.js
+++ App.js
@@ -1,37 +1,28 @@
-import { StatusBar, useState } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import * as Font from 'expo-font'
 import AppLoading from 'expo-app-loading'
+import MealsNavigator from './navigation/MealsNavigation'
 
 const fetchFonts = () => {
-  Font.loadAsync({
+  return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   });
-}
+};
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false)
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-  if(!fontLoaded) {
-    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn} />
+    );
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MealsNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+import CATEGORIES from '../data/dummy-data'
 
 const CategoryMealsScreen = props => {
+    const catId = props.navigation.getParams('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
     return (
         <View style={styles.screen}>
             <Text>The Category Meals Screen</Text>
+            <Button title="Go to details" onPress={() => {
+                props.navigation.navigate({
+                    routeName: 'MealDetail'
+                });
+            }}/>
+            <Button title="replace with category" onPress={() => {
+                props.navigation.replace({routeName: 'CategoryMeals'})
+            }}/>
+            <Button title="Go back" onPress={() => {
+                props.navigation.pop();
+            }}/>
+            
         </View>
     );
 };
