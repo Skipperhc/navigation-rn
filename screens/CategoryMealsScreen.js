985fb62850a9ecc24612231781bd05344e9450b4
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2021-05-14 17:12:22 -0300

    Navegando passando dados

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,28 +1,22 @@
 import React from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 
-import CATEGORIES from '../data/dummy-data'
+import { CATEGORIES } from '../data/dummy-data'
 
 const CategoryMealsScreen = props => {
-    const catId = props.navigation.getParams('categoryId');
+    const catId = props.navigation.getParam('categoryId');
 
     const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
     return (
         <View style={styles.screen}>
             <Text>The Category Meals Screen</Text>
+            <Text>{selectedCategory.title}</Text>
             <Button title="Go to details" onPress={() => {
                 props.navigation.navigate({
                     routeName: 'MealDetail'
                 });
-            }}/>
-            <Button title="replace with category" onPress={() => {
-                props.navigation.replace({routeName: 'CategoryMeals'})
-            }}/>
-            <Button title="Go back" onPress={() => {
-                props.navigation.pop();
-            }}/>
-            
+            }}/>            
         </View>
     );
 };
