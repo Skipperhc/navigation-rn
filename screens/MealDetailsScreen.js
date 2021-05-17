4a23a74208d9911cacbee35804a96f5b8eee31d4
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2021-05-17 16:56:44 -0300

    Adicionando botão favoritos ao header

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-import { MEALS } from '../data/dummy-data'
+import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailsScreen = props => {
     const mealId = props.navigation.getParam('mealId');
@@ -13,7 +15,7 @@ const MealDetailsScreen = props => {
             <Text>{selectedMeal.title}</Text>
             <Button title="Go back to categories" onPress={() => {
                 props.navigation.popToTop();
-            }}/>
+            }} />
         </View>
     );
 };
@@ -22,7 +24,20 @@ MealDetailsScreen.navigationOptions = (navigationData) => {
     const mealId = navigationData.navigation.getParam('mealId');
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return {
-        headerTitle: selectedMeal.title
+        headerTitle: selectedMeal.title,
+        headerRight: () => {
+            return (
+                <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                    <Item
+                        title='Favorite'
+                        iconName='ios-star'
+                        onPress={() => {
+                            console.log('Mark as favorite');
+                        }}
+                    />
+                </HeaderButtons>
+            )
+        }
     }
 }
 
