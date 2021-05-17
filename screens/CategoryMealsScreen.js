9dd23ddd2a0b3223001b867795f69ca0f9ae56bf
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2021-05-17 10:55:18 -0300

    mostrando apenas pratos da categoria

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 const CategoryMealsScreen = props => {
+    const renderMealItem = itemData => {
+        return (
+            <View><Text>{itemData.item.title}</Text></View>
+        );
+    }
+
     const catId = props.navigation.getParam('categoryId');
 
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+    const displayMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
 
     return (
         <View style={styles.screen}>
-            <Text>The Category Meals Screen</Text>
-            <Text>{selectedCategory.title}</Text>
-            <Button title="Go to details" onPress={() => {
-                props.navigation.navigate({
-                    routeName: 'MealDetail'
-                });
-            }}/>            
+            <FlatList
+                data={displayMeals}
+                keyExtractor={(item, index) => item.id}
+                renderItem={renderMealItem} />
         </View>
     );
 };
